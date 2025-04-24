93e674c60973c352564919c06cc1b37e65dd464c
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-04-25 01:52:44 +0800

    feat: download-url

--- internal/flags/init_flag.go
+++ internal/flags/init_flag.go
@@ -8,24 +8,26 @@ import (
 )
 
 var Config struct {
-	Path    string
-	Version string
-	Proxy   string
-	Debug   bool
+	Path        string
+	Version     string
+	Proxy       string
+	DownloadURL string
+	Debug       bool
 }
 
 func InitFlag() bool {
 	path := "./"
 	flag.StringVar(&path, "path", "./", "NapCat path")
+	flag.StringVar(&Config.Version, "version", "", "Update NapCat Version")
+	flag.StringVar(&Config.Proxy, "proxy", "", "HTTP Proxy")
+	flag.StringVar(&Config.DownloadURL, "download-url", "", "Download NapCat URL")
+	flag.BoolVar(&Config.Debug, "debug", true, "Enable debug logging")
+	flag.Parse()
 	var err error
 	Config.Path, err = filepath.Abs(path)
 	if err != nil {
 		Config.Path = path
 		log.RPanic(err)
 	}
-	flag.StringVar(&Config.Version, "version", "", "Update NapCat Version")
-	flag.StringVar(&Config.Proxy, "proxy", "", "HTTP Proxy")
-	flag.BoolVar(&Config.Debug, "debug", true, "Enable debug logging")
-	flag.Parse()
 	return true
 }
